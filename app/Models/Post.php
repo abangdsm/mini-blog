2ddd27ae2a24b8de2fbe6709416851e563410cbd
abangdsm <id.reversal@gmail.com>
2024-08-08 18:09:47 +0700

    Menambahkan Category dan Post Model

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'author',
+        'article'
+    ];
+
+    public function category(): HasMany
+    {
+        return $this->hasMany(Category::class, 'category_id');
+    }
+}
